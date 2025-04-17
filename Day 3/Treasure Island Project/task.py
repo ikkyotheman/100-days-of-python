def5cf125dc286cb5b12e63d7a2538c08ce9a794
ikkyotheman <pkindrachuk9@gmail.com>
2025-04-16 19:24:34 -0700

    Treasure island update

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -22,3 +22,23 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
+direction = input("You're at a crossroads. Where do you want to go? Type 'left' or 'right'.")
+if direction == "left" or direction == "LEFT" or direction == "Left":
+    print("you've made it safely to an island")
+else:
+    print("you got hit by a bus foo")
+    direction_2 = input("You realize that the island is sinking. What do you do? Type 'Swim' or 'Wait'.")
+    if direction_2 == "wait" or direction_2 == "Wait" or direction_2 == "WAIT":
+        print("The island has started to emerge from the water. You notice a large box with 3 sides sticking out of the wet sand on the shore")
+    else:
+        print("you've been devoured by sea snakes. GAME OVER")
+    direction_3 = input("Each side of the box is a different color; Red, Blue and Yellow. Type which color you open")
+    if direction_3 == "red" or direction_3 == "Red" or direction_3 == "RED":
+        print("You are burned by fire. GAME OVER")
+    elif direction_3 == "blue" or direction_3 == "Blue" or direction_3 == "BLUE":
+        print("You've been eaten by beasts. GAME OVER")
+    elif direction_3 == "yellow" or direction_3 == "Yellow" or direction_3 == "YELLOW":
+        print("YOU WIN")
+else:
+    print("GAME OVER!")
+
