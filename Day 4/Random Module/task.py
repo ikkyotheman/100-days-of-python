8168532f3c48d644f64b3aa4d847a61a0c502c90
ikkyotheman <pkindrachuk9@gmail.com>
2025-04-21 18:30:19 -0700

    Updating 4/21 from work - Rock, Paper, Scissors

--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -2,8 +2,14 @@ import random
 
 # random_integer = random.randint(1, 10)
 # print(random_integer)
-random_number_0_to_1 = random.random() * 10
-print(random_number_0_to_1)
+# random_number_0_to_1 = random.random() * 10
+# print(random_number_0_to_1)
 
-random_float = random.uniform(1,10)
-print(random_float)
+# random_float = random.uniform(1,10)
+# print(random_float)
+
+random_integer = random.randint(0,1)
+if random_integer == 1:
+    print("Heads")
+else:
+    print("Tails")
--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -24,3 +24,24 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+your_hand = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors"))
+if your_hand == 0:
+    print(rock)
+if your_hand == 1:
+    print(paper)
+if your_hand == 2:
+    print(scissors)
+import random
+random_int = random.randint(0,2)
+if random_int == 0:
+    print(f"Computer chose: {rock}")
+if random_int == 1:
+    print(f"Computer chose: {paper}")
+if random_int == 2:
+    print(f"Computer chose: {scissors}")
+if your_hand == random_int:
+    print("Tie")
+if (your_hand == 0 and random_int == 1) or (your_hand == 1 and random_int == 2) or (your_hand == 2 and random_int == 0):
+    print("You Lose")
+if (your_hand == 0 and random_int == 2) or (your_hand == 1 and random_int == 0) or (your_hand == 2 and random_int == 1):
+    print("You win")
