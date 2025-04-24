59218051d3b600965e4182f0829b99778021941f
ikkyotheman <pkindrachuk9@gmail.com>
2025-04-23 21:58:09 -0700

    commit

--- Day 5/For Loops/task.py
+++ Day 5/For Loops/task.py
@@ -1,1 +1,8 @@
 fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+for day in days:
+    print(day)
+    print(days)
