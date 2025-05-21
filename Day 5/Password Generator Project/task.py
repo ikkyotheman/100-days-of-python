0d4d6f788c826987867edd0fb783eace2ad5e848
ikkyotheman <pkindrachuk9@gmail.com>
2025-05-20 18:42:41 -0700

    Commit at work for password generator project FINALLY!

--- Day 5/Password Generator Project/task.py
+++ Day 5/Password Generator Project/task.py
@@ -8,7 +8,6 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-
 # Easy Level
 # password = ""
 # for letter_input in range(0, nr_letters):
