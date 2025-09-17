ce89bd339675ac80b59ed48c034ddac88fa32ea3
pkindrachuk <pk.wav.404@gmail.com>
2025-09-16 18:35:51 -0700

    Updates from Calcium

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,4 +1,21 @@
 word_list = ["aardvark", "baboon", "camel"]
+import random
+chosen_word = random.choice(word_list)
+print(chosen_word)
+chosen_length = len(chosen_word)
+guess = input("Guess a letter").lower()
+for each_letter in chosen_word:
+    if each_letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+
+
+# if guess == chosen_word[chosen_length-1]:
+#     print("Correct!")
+
+# You got this but confused why the string works like a list. Look that up..up hint 2 says you can do that.
+
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 
