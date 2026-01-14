9ee9c594f7266765e40d1ef2d523e84c825519c7
peterkindrachuk <pkindrachuk9@gmail.com>
2026-01-13 21:59:53 -0800

    update from macbook

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -1,7 +1,29 @@
 from art import logo
+from art import vs
 import game_data
+import random
 print(logo)
+# Here we select the two people
+selectionFirst = random.choice(game_data.data)
+selectionSecond = random.choice(game_data.data)
+print(selectionFirst)
+print(selectionSecond)
+# Create a function that compares the followers of the two choices.
+def compare(first, second):
+    if selectionFirst['followers'] > selectionSecond['followers']:
 
-print(game_data.data)
-for key in game_data.data:
-    print(key)
+# TODO the it asks "A, Name, Description and Country of Origin"
+print(f"Compare A: {selectionFirst['name']}, a {selectionFirst['description']}, from {selectionFirst['country']}.")
+
+# TODO need to make it so once a choice is selected from the pool, it can't be selected again (A and B were cardi B)
+
+# TODO between A and B there is a large "VS." logo
+print(vs)
+
+print(f"Against B: {selectionSecond['name']}, a {selectionSecond['description']}, from {selectionSecond['country']}.")
+# TODO It asks to select A or B.
+userGuess = input("Who has more followers? Type 'A' or 'B': ")
+# if userGuess == 'A':
+
+# TODO When answered correctly, that selection becomes "A" and the B is a new choice.
+# TODO losing will give you the art and below saying "Sorry, that's wrong. Final score: X" with "x" being how far you got (2 or whatever)
