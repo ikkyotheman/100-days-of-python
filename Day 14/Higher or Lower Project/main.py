1d3a8b935d62b94b01437f82fbb9456f9d38ca36
peterkindrachuk <pkindrachuk9@gmail.com>
2026-01-14 16:27:37 -0800

    update from macbook

--- Day 14/Higher or Lower Project/main.py
+++ Day 14/Higher or Lower Project/main.py
@@ -3,31 +3,44 @@ from art import vs
 import game_data
 import random
 print(logo)
-# Here we select the two people
-# TODO need to make it so once a choice is selected from the pool, it can't be selected again (A and B were cardi B)
+USERSCORE = 0
 selectionFirst = random.choice(game_data.data)
 game_data.data.remove(selectionFirst)
 selectionSecond = random.choice(game_data.data)
-game_data.data.remove(selectionSecond)
-print(selectionFirst)
-print(selectionSecond)
-print(game_data.data) # This shows that the selections have been removed.
+game_data.data.remove(selectionSecond) # Two choices are randomly selected and removed from the dictionary
 
-# TODO Create a function that compares the followers of the two choices.
-def compare(selectionFirst, ):
-#     if selectionFirst['followers'] > selectionSecond['followers']:
-#
-# # TODO the it asks "A, Name, Description and Country of Origin"
-# print(f"Compare A: {selectionFirst['name']}, a {selectionFirst['description']}, from {selectionFirst['country']}.")
-#
 
-#
-# # TODO between A and B there is a large "VS." logo
-# print(vs)
-# print(f"Against B: {selectionSecond['name']}, a {selectionSecond['description']}, from {selectionSecond['country']}.")
-# # TODO It asks to select A or B.
-# userGuess = input("Who has more followers? Type 'A' or 'B': ")
-# # if userGuess == 'A':
-#
-# # TODO When answered correctly, that selection becomes "A" and the B is a new choice.
-# # TODO losing will give you the art and below saying "Sorry, that's wrong. Final score: X" with "x" being how far you got (2 or whatever)
+# TODO Create a function that compares the follower_count of the two choices.
+def compare(selection):
+    print(selectionFirst['name'], selectionFirst['follower_count']) #This is for checking it's mathing correctly
+    print(selectionSecond['name'], selectionSecond['follower_count'])
+    if selection == "A":
+        if selectionFirst['follower_count'] > selectionSecond['follower_count']:
+            USERSCORE += 1
+            print("You win!")
+            print(f"Your score is: {USERSCORE}")
+        else:
+            print("You lose!")
+            print(f"Your score is: {USERSCORE}")
+    elif selection == "B":
+        if selectionSecond['follower_count'] > selectionFirst['follower_count']:
+            print("You Win!")
+            USERSCORE += 1
+            print(f"Your score is: {USERSCORE}")
+        else:
+            print("You Lose!")
+            print(f"Your score is: {USERSCORE}")
+
+
+# TODO the it asks "A, Name, Description and Country of Origin"
+print(f"Compare A: {selectionFirst['name']}, a {selectionFirst['description']}, from {selectionFirst['country']}.")
+
+
+print(vs) # This prints the vs sign between the two options
+print(f"Against B: {selectionSecond['name']}, a {selectionSecond['description']}, from {selectionSecond['country']}.")
+# TODO It asks to select A or B.
+compare(input("Who has more follower_count? Type 'A' or 'B': ").upper())
+# if userGuess == 'A':
+
+# TODO When answered correctly, that selection becomes "A" and the B is a new choice.
+# TODO losing will give you the art and below saying "Sorry, that's wrong. Final score: X" with "x" being how far you got (2 or whatever)
